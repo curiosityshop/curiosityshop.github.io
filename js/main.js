c4036bbb3e1be30cfd81e677de2c75a9cacbfd8e
DrozdovNikolai <112856511+DrozdovNikolai@users.noreply.github.com>
2023-01-23 10:23:17 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -84,43 +84,45 @@ $(document).ready(function () {
 
   /* Отправка формы */
 
-  let names = document.getElementById("InputName");
-  let phone = document.getElementById("InputPhone");
-  let email = document.getElementById("InputEmail");
-  let msg = document.getElementById("Textarea");
-  let cb = document.getElementById("Check");
-
-  function save() {
-    localStorage.setItem("Имя", names.value);
-    localStorage.setItem("Телефон",phone.value)
-    localStorage.setItem("Почта", email.value);
-    localStorage.setItem("Сообщение", msg.value);
-    if (cb.checked) {
-        localStorage.setItem('Чекбокс', 1);
-      } else {
-        localStorage.setItem('Чекбокс', 0);
-      }
-  }
-  document.addEventListener("DOMContentLoaded", function (event) {
-    names.value = localStorage.getItem("Имя");
-    phone.value = localStorage.getItem("Телефон");
-    email.value = localStorage.getItem("Почта");
-    msg.value = localStorage.getItem("Сообщение");
-    let checkBox = localStorage.getItem('Чекбокс');
-    if (checkBox == 1) {
-      cb.checked = true;
-    } else if (checkBox == 0) {
-      cb.checked = false;
-    }
   
-    names.oninput = save;
-    phone.oninput = save;
-    email.oninput = save;
-    msg.oninput = save;
-    cb.onclick = save;
+  document.addEventListener("DOMContentLoaded", function (event) {
+
   });
 
   $(function () {
+    let names = document.getElementById("InputName");
+    let phone = document.getElementById("InputPhone");
+    let email = document.getElementById("InputEmail");
+    let msg = document.getElementById("Textarea");
+    let cb = document.getElementById("Check");
+  
+    function save() {
+      localStorage.setItem("Имя", names.value);
+      localStorage.setItem("Телефон",phone.value)
+      localStorage.setItem("Почта", email.value);
+      localStorage.setItem("Сообщение", msg.value);
+      if (cb.checked) {
+          localStorage.setItem('Чекбокс', 1);
+        } else {
+          localStorage.setItem('Чекбокс', 0);
+        }
+    }
+      names.value = localStorage.getItem("Имя");
+      phone.value = localStorage.getItem("Телефон");
+      email.value = localStorage.getItem("Почта");
+      msg.value = localStorage.getItem("Сообщение");
+      let checkBox = localStorage.getItem('Чекбокс');
+      if (checkBox == 1) {
+        cb.checked = true;
+      } else if (checkBox == 0) {
+        cb.checked = false;
+      }
+    
+      names.oninput = save;
+      phone.oninput = save;
+      email.oninput = save;
+      msg.oninput = save;
+      cb.onclick = save;
     $(".ajaxForm").submit(function (e) {
       e.preventDefault();
       var href = $(this).attr("action");
@@ -149,5 +151,6 @@ $(document).ready(function () {
       });
     });
   });
+
 
 
